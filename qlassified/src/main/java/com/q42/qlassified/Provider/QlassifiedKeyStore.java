bda8dd2ce2bff47ae4e1bbf1c56bb0aa913b3ebc
Eloi Navarro <reo.developer@gmail.com>
2019-05-24 22:18:20 +0200

    Fix KeyStore exception on android 9+

--- qlassified/src/main/java/com/q42/qlassified/Provider/QlassifiedKeyStore.java
+++ qlassified/src/main/java/com/q42/qlassified/Provider/QlassifiedKeyStore.java
@@ -28,6 +28,7 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
 import java.security.interfaces.RSAPublicKey;
@@ -90,11 +91,12 @@ public class QlassifiedKeyStore implements QlassifiedSecurity {
                             alias,
                             KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
                             .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
-                            .setAlgorithmParameterSpec(new RSAKeyGenParameterSpec(512, RSAKeyGenParameterSpec.F4))
+                            .setAlgorithmParameterSpec(
+                                    new RSAKeyGenParameterSpec(512, RSAKeyGenParameterSpec.F4))
                             .build());
-        /**
-         * On versions below Marshmellow but above Jelly Bean, use the next best thing
-         */
+            /**
+             * On versions below Marshmellow but above Jelly Bean, use the next best thing
+             */
         } else {
 
             Calendar start = new GregorianCalendar();
@@ -103,12 +105,14 @@ public class QlassifiedKeyStore implements QlassifiedSecurity {
 
             KeyPairGeneratorSpec keyPairGeneratorSpec =
                     new KeyPairGeneratorSpec.Builder(context)
-                            // You'll use the alias later to retrieve the key.  It's a key for the key!
+                            // You'll use the alias later to retrieve the key.  It's a key for
+                            // the key!
                             .setAlias(alias)
-                                    // The subject used for the self-signed certificate of the generated pair
+                            // The subject used for the self-signed certificate of the generated
+                            // pair
                             .setSubject(new X500Principal("CN=" + alias))
-                                    // The serial number used for the self-signed certificate of the
-                                    // generated pair.
+                            // The serial number used for the self-signed certificate of the
+                            // generated pair.
                             .setSerialNumber(BigInteger.valueOf(1337))
                             .setStartDate(start.getTime())
                             .setEndDate(end.getTime())
@@ -117,10 +121,10 @@ public class QlassifiedKeyStore implements QlassifiedSecurity {
             keyPairGenerator = KeyPairGenerator
                     .getInstance(KeyProperties.KEY_ALGORITHM_RSA, ANDROID_KEYSTORE_INSTANCE);
             keyPairGenerator.initialize(keyPairGeneratorSpec);
-        /**
-         * On versions below that...
-         * Well we're sorry but you don't get a fancy encryption baby...
-         */
+            /**
+             * On versions below that...
+             * Well we're sorry but you don't get a fancy encryption baby...
+             */
         }
 
         KeyPair keyPair = keyPairGenerator.generateKeyPair();
@@ -141,11 +145,11 @@ public class QlassifiedKeyStore implements QlassifiedSecurity {
 
     public QlassifiedEntry decryptEntry(EncryptedEntry entry) {
         final String decryptedString = decrypt(entry.getEncryptedValue());
-        if(decryptedString == null) {
+        if (decryptedString == null) {
             return null;
         }
         final Integer splitPosition = decryptedString.lastIndexOf(TYPE_DELIMITER);
-        if(splitPosition == -1) {
+        if (splitPosition == -1) {
             return null;
         }
         final String decryptedType = decryptedString.substring(splitPosition + 1);
@@ -154,11 +158,16 @@ public class QlassifiedKeyStore implements QlassifiedSecurity {
         final String key = entry.getKey();
 
         switch (QlassifiedEntry.Type.valueOf(decryptedType)) {
-            case BOOLEAN: return new QlassifiedBoolean(key, Boolean.valueOf(decryptedValue));
-            case FLOAT: return new QlassifiedFloat(key, Float.valueOf(decryptedValue));
-            case INTEGER: return new QlassifiedInteger(key, Integer.valueOf(decryptedValue));
-            case LONG: return new QlassifiedLong(key, Long.valueOf(decryptedValue));
-            default: return new QlassifiedString(key, decryptedValue);
+            case BOOLEAN:
+                return new QlassifiedBoolean(key, Boolean.valueOf(decryptedValue));
+            case FLOAT:
+                return new QlassifiedFloat(key, Float.valueOf(decryptedValue));
+            case INTEGER:
+                return new QlassifiedInteger(key, Integer.valueOf(decryptedValue));
+            case LONG:
+                return new QlassifiedLong(key, Long.valueOf(decryptedValue));
+            default:
+                return new QlassifiedString(key, decryptedValue);
         }
     }
 
@@ -167,7 +176,7 @@ public class QlassifiedKeyStore implements QlassifiedSecurity {
         String alias = getUniqueDeviceId(this.context);
         // Create keys based on the unique device identifier
         try {
-            if(!keyStoreInstance.containsAlias(alias)) {
+            if (!keyStoreInstance.containsAlias(alias)) {
                 createKeys();
             }
             return true;
@@ -176,45 +185,69 @@ public class QlassifiedKeyStore implements QlassifiedSecurity {
                 NoSuchAlgorithmException |
                 InvalidKeyException |
                 InvalidAlgorithmParameterException e) {
-            Logger.e("QlassifiedKeyStore", String.format("Could not create a KeyStore instance. Stacktrace: %s", e));
+            Logger.e("QlassifiedKeyStore",
+                    String.format("Could not create a KeyStore instance. Stacktrace: %s", e));
             return false;
         }
     }
 
     private String encrypt(String input) {
 
-        if(!checkKeyAvailability()) {
+        if (!checkKeyAvailability()) {
             return null;
         }
 
         String alias = getUniqueDeviceId(this.context);
         try {
-            final KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) keyStoreInstance.getEntry(alias, null);
-            final RSAPublicKey publicKey = (RSAPublicKey) privateKeyEntry.getCertificate().getPublicKey();
+            KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+
+            final PrivateKey privateKeyEntry = (PrivateKey) keyStore.getKey(alias, null);
+            final RSAPublicKey publicKey = (RSAPublicKey)keyStore.getCertificate(alias).getPublicKey();
             return crypto.encrypt(input, publicKey);
         } catch (NoSuchAlgorithmException |
                 UnrecoverableEntryException |
                 KeyStoreException e) {
-            Logger.e("QlassifiedKeyStore", String.format("Could not encrypt this string. Stacktrace: %s", e));
+            Logger.e("QlassifiedKeyStore",
+                    String.format("Could not encrypt this string. Stacktrace: %s", e));
+            return null;
+        } catch (CertificateException e) {
+            Logger.e("QlassifiedKeyStore",
+                    String.format("Could not encrypt this string. Stacktrace: %s", e));
+            return null;
+        } catch (IOException e) {
+            Logger.e("QlassifiedKeyStore",
+                    String.format("Could not encrypt this string. Stacktrace: %s", e));
             return null;
         }
     }
 
     private String decrypt(String input) {
 
-        if(!checkKeyAvailability()) {
+        if (!checkKeyAvailability()) {
             return null;
         }
 
         String alias = getUniqueDeviceId(this.context);
         try {
-            KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry)keyStoreInstance.getEntry(alias, null);
-            PrivateKey privateKey =  privateKeyEntry.getPrivateKey();
+            KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+
+            PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, null);
             return crypto.decrypt(input, privateKey);
         } catch (NoSuchAlgorithmException |
                 UnrecoverableEntryException |
                 KeyStoreException e) {
-            Logger.e("QlassifiedKeyStore", String.format("Could not decrypt this string. Stacktrace: %s", e));
+            Logger.e("QlassifiedKeyStore",
+                    String.format("Could not decrypt this string. Stacktrace: %s", e));
+            return null;
+        } catch (CertificateException e) {
+            Logger.e("QlassifiedKeyStore",
+                    String.format("Could not decrypt this string. Stacktrace: %s", e));
+            return null;
+        } catch (IOException e) {
+            Logger.e("QlassifiedKeyStore",
+                    String.format("Could not decrypt this string. Stacktrace: %s", e));
             return null;
         }
     }
